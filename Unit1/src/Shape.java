b83531afa81d1a6eab9842810c17e31871db56a7
Dean Mason <dean.hj.mason@gmail.com>
2025-06-23 11:59:21 -0400

    Started

---
+++ Unit1/src/Shape.java
@@ -0,0 +1,17 @@
+//Dean Mason
+//Unit1
+
+import java.awt.*;
+
+public abstract class Shape {
+    /** First point*/
+    private Point point1;
+    /** Second point*/
+    private Point point2;
+    /** Color value*/
+    private double color;
+
+    public void draw(Graphics g) {
+    //Empty for now, implemented in future classes
+    }
+}
