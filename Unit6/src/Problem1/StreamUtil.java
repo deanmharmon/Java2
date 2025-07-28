e6b202bd3eef2e473e21d4de36359eb8599446be
Dean Mason <dean.hj.mason@gmail.com>
2025-07-28 03:40:53 -0400

    Finished 1, actually

--- Unit6/src/Problem1/StreamUtil.java
+++ Unit6/src/Problem1/StreamUtil.java
@@ -15,7 +15,7 @@ public class StreamUtil
 {
    /**
     * Measures the time it takes to count the number of odd numbers using
-    * an infinite stream, given a limit. 
+    * an infinite stream, given a limit of numbers to count.
     * @param generator the Random generator used to generate the numbers.
     * @param limit the number of odd numbers to count
     * @param isParallel if true, use a parallel stream
@@ -35,26 +35,26 @@ public class StreamUtil
               .filter(w -> w % 2 != 0)
               .limit(limit)
               .count();
-
+      //System.out.println(totalOddInt);
 
        return (System.nanoTime() - start);
    }
 }
 
 /**
- * Main runs the program
+ * Main runs the program. I honestly didn't look at other file until after figuring this out since it wasn't referenced here
+ * and it didn't say it needed any code in the rubric. This is neat to see the timing differences. But the other runs as well.
  */
 class Main {
    public static void main(String[] args) {
       Random generator = new Random();
-
       int run = 10;
       while(run < 100000000){
          long regular = StreamUtil.timeOdds(generator, run, false);
          long parallel = StreamUtil.timeOdds(generator, run, true);
          double a = (double) regular / 1000000;
          double b = (double) parallel / 1000000;
-         System.out.println("(Nanoesecond) Regular time for " + run + ": " + regular + " || " + "Parallel time for " + run + ": " + parallel);
+         System.out.println("(Nanosecond) Regular time for " + run + ": " + regular + " || " + "Parallel time for " + run + ": " + parallel);
          System.out.println("\t\t\t\tMillisecond time: " + a + "\t||\t" + b);
          run = run * 10;
 
